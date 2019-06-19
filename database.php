39839c3d2b19f45cf7a4c21bec7ca5f86ba0ddae
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-06-19 21:13:17 +0200

    Connecting to DB

--- database.php
+++ database.php
@@ -1,7 +1,13 @@
 <?php 
-$conn = mysqli_connect('localhost', 'root', '', 'quizzer');
+$db_host = 'localhost';
+$db_name = 'quizzer';
+$db_user = 'root';
+$db_pass = (string)'';
 
-if (mysqli_errno()) {
-    die("Failed to connect to MySQL: " . mysqli_error());
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_error) {
+    printf("Connection Failed : %s\n", $mysqli->connect_error);
+    exit();
 }
 ?>
